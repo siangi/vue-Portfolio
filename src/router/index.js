2b2e39d8a4e28064b63205c2d193c072c3810f44
siangi <siangi@gmx.ch>
2022-11-10 14:30:23 +0100

    about view is about view again

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
-      component: HomeView,
+      component: AboutView,
     },
     {
       path: "/detail/:id",
